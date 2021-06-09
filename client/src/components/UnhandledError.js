67aa19e7a6d99c1c647ce788202c5788548904c4
TamikaHayes <tamika.hayes@gmail.com>
2021-06-09 11:54:22 -0700

    Added Forbidden and UnhandledError components

---
+++ client/src/components/UnhandledError.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ const UnhandledError = () => (
+  <div className="wrap">
+    <h1>Error</h1>
+    <p> We just encountered an unexpected error.</p>
+  </div>
+);
+
+export default UnhandledError;
